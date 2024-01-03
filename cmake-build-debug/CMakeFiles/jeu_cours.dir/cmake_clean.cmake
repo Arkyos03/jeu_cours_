9922775ca0347f4add338591b1bce646854601c0
Arkyos03 <12damienf@gmail.com>
2024-01-03 14:22:52 +0100

    1er

---
+++ cmake-build-debug/CMakeFiles/jeu_cours.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jeu_cours.dir/main.cpp.obj"
+  "jeu_cours.exe"
+  "jeu_cours.exe.manifest"
+  "jeu_cours.pdb"
+  "libjeu_cours.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jeu_cours.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
